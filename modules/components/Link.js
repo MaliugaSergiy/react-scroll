5287427a4e2c457a3dea4e9e78d014368c68c192
Maliuga Sergiy <shtirliy@gmail.com>
2018-10-12 14:04:05 +0300

    delete logs

--- modules/components/Link.js
+++ modules/components/Link.js
@@ -5,7 +5,6 @@ import ScrollLink from "../mixins/scroll-link";
 
 class LinkElement extends React.Component {
   render() {
-    console.log(this.props);
     return <a {...this.props}>{this.props.children}</a>;
   }
 }
